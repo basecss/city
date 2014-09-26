01384e661b481f1a839f3f68aa7364bb15e12516
TooBug <i@toobug.net>
2014-09-26 14:38:21 +0800

    整理文件，从统计局网站爬最新数据

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var commander = require('commander');
+var version = require('./package.json').version;
+var generator = require('./lib/generator.js');
+
+commander
+	.version(version)
+	.option('-l --level','输出指定级别行政区数据')
+	.option('-s --overseas','包含海外地区')
+	.option('-t --type','输出类型，array/object')
+	// .option('-k --key','对象键名，name/zipcode/')
+	.option('-f --flatten','扁平化输出')
+	.option('-z --zipcode','包含邮编')
+	.option('-u --update','重新抓取原始数据')
+	.parse(process.argv);
+
+
+if(commander.update){
+	require('./lib/update').update();
+}else{
+	var options = commander;
+	generator.generate(options);
+}
