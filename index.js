678b13addb702a9850c11080aee5bd73068e556c
TooBug <i@toobug.net>
2014-09-26 19:23:44 +0800

    生成一二三级数据功能完成

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ var generator = require('./lib/generator.js');
 
 commander
 	.version(version)
-	.option('-l --level','输出指定级别行政区数据')
-	.option('-s --overseas','包含海外地区')
-	.option('-t --type','输出类型，array/object')
+	.option('-l --level <n>','输出指定级别行政区数据',parseInt)
+	// .option('-s --overseas','包含海外地区')
+	// .option('-t --type','输出类型，array/object')
 	// .option('-k --key','对象键名，name/zipcode/')
-	.option('-f --flatten','扁平化输出')
-	.option('-z --zipcode','包含邮编')
+	// .option('-f --flatten','扁平化输出')
+	// .option('-z --zipcode','包含邮编')
+	.option('-c --code','包含地区编码（身份证前6位）')
 	.option('-u --update','重新抓取原始数据')
 	.parse(process.argv);
 
@@ -19,6 +20,9 @@ commander
 if(commander.update){
 	require('./lib/update').update();
 }else{
-	var options = commander;
+	var options = {
+		level:commander.level,
+		code:commander.code
+	};
 	generator.generate(options);
 }
